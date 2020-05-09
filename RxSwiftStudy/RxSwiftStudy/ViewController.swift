433008b974adc40cb81760b5bba8be6f16193f0e
denny.k <della.kimko@gmail.com>
2020-05-09 11:48:40 +0900

    Basic Observable Object, Subscription

--- RxSwiftStudy/RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/RxSwiftStudy/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        observableTest()
     }
 
+    func observableTest() {
+        let observable = Observable<Int>.range(start: 0, count: 10)
+        observable.subscribe(onNext: { (i) in
+            let n = Double(i)
+            let fibonacci = Int(((pow(1.61803, n) - pow(0.61803, n)) / 2.23606).rounded())
+            print(fibonacci)
+        }, onCompleted: {
+            print("Completed")
+        })
+        
+        let observable2 = Observable.of("A", "B", "C")
+        let subscription = observable2.subscribe({ (event) in
+            print(event)
+        })
+        subscription.dispose()
+    }
 
 }
 
